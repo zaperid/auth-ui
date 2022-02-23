aeb3e768234a8270778051859bfeb4aa3b4dd133
Aza Maulana <azamaulanaaa@gmail.com>
2022-02-23 23:06:11 +0700

    feat(next): use next layout mechanism

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 
 import '@fontsource/roboto/300.css';
@@ -5,6 +7,16 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+type NextPageWithLayout = NextPage & {
+    getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+    const getLayout = Component.getLayout ?? ((page) => page);
+
+    return getLayout(<Component {...pageProps} />);
 }
